171dec781abd5227cc53d908e0c0a1d1b2dcbab7
Rubén Montero Ruiz <rumonru05@gmail.com>
2026-05-31 15:11:12 +0200

    Renaming file: plantilla_campos_potenciales.m to campos_potenciales.m

--- campos_potenciales.m
+++ campos_potenciales.m
@@ -26,10 +26,10 @@ max_rango=10;
 angulos=-pi/2:(pi/180):pi/2; % resolucion angular barrido laser
 
 % Caracteristicas del vehiculo y parametros del metodo
-v=0.4;            % Velocidad del robot
-D=1.5;           % Rango del efecto del campo de repulsión de los obstáculos
+v=1;            % Velocidad del robot
+D=5;           % Rango del efecto del campo de repulsión de los obstáculos
 alfa=1;           % Coeficiente de la componente de atracción
-beta=100;      % Coeficiente de la componente de repulsión
+beta=200;      % Coeficiente de la componente de repulsión
 
 %% Inicialización
 
@@ -41,12 +41,54 @@ iteracion=0;              % Se controla el nº de iteraciones por si se entra en
 %% Calculo de la trayectoria
 
 while norm(destino-robot(1:2)) > v && iteracion<1000    % Hasta menos de una iteración de la meta (10 cm)
-   % TU CODIGO AQUI %%%%%%%%%%%%%
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    
+    % 1. Obtener las lecturas del sensor Lidar (obstáculos locales)
+    obs = SimulaLidar(robot, mapa, angulos, max_rango);
+    
+    % 2. Calcular la Fuerza de Atracción (hacia el destino)
+    vec_atr = destino - robot(1:2);
+    dist_dest = norm(vec_atr);
+    
+    % Fuerza proporcional a la distancia
+    F_atr = alfa * vec_atr;
+    
+    % 3. Calcular la Fuerza de Repulsión (alejándose de los obstáculos)
+    F_rep = [0, 0];
+    
+    % Iterar sobre cada rayo del Lidar
+    for i = 1:size(obs, 1)
 
+        if ~isnan(obs(i,1)) 
 
+            vec_rep = robot(1:2) - obs(i, :);
+            dist_obs = norm(vec_rep);
+            
+            if dist_obs < D && dist_obs > 0
 
-   
+                u_rep = vec_rep / dist_obs;
+                
+                mag_rep = beta * ((1/dist_obs) - (1/D)) * (1/(dist_obs^2));
+                
+                % Sumatorio de las fuerzas de repulsión
+                F_rep = F_rep + (mag_rep * u_rep);
+            end
+        end
+    end
+    
+    % 4. Calcular la Fuerza Resultante
+    F_res = F_atr + F_rep;
+    
+    % 5. Actualizar la posición y orientación del robot
+    theta = atan2(F_res(2), F_res(1));
+    
+    % Actualización x, y, theta
+    robot(1) = robot(1) + v * cos(theta);
+    robot(2) = robot(2) + v * sin(theta);
+    robot(3) = theta;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   
 
     path = [path;robot];	% Se añade la nueva posición al camino seguido
     plot(path(:,1),path(:,2),'r');
